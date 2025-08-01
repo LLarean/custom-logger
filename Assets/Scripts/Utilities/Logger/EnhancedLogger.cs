813295a7729edde7c672f6ae7ed5fe5e2cf05d5d
Stanislav Kozlov <llarean@ya.ru>
2025-08-01 14:43:30 +0300

    Upgrade logger with enums and safety checks
    Enum log levels.
    Null checks.
    Async support.
    Perf optimizations.

--- Assets/Scripts/Utilities/Logger/EnhancedLogger.cs
+++ Assets/Scripts/Utilities/Logger/EnhancedLogger.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Debug = UnityEngine.Debug;
 
 namespace DebugTools
@@ -19,67 +21,90 @@ namespace DebugTools
         /// <summary>
         /// The minimum level of importance of the logs displayed, 0 only critical, 1 only important and critical, 2 all logs
         /// </summary>
-        private const int LogDisplayLevel = 2;
+        private const LogLevel LogDisplayLevel = LogLevel.Verbose;
+        
+        private static string _lastCallerData;
+        private static string _lastMessage;
         
         /// <summary>
         /// Output a regular message to the console
         /// </summary>
-        /// <param name="message">Message to the console</param>
-        /// <param name="importanceLevel">0 is critical, 1 is necessary to pay attention, 2 is the most unimportant</param>
-        public static void Log(string message, int  importanceLevel = 2)
+        /// <param name="messageText">Message to the console</param>
+        /// <param name="importanceLevel">-</param>
+        public static void Log(string messageText, LogLevel importanceLevel = LogLevel.Verbose)
         {
-            if(IsDevelopmentModeEnabled == false) return;
+            if(!IsDevelopmentModeEnabled) return;
             
             if(LogDisplayLevel < importanceLevel) return;
             
-            Debug.Log($"{GetCallerData()}'{message}'");
+            if (string.IsNullOrEmpty(messageText)) throw new ArgumentNullException(nameof(messageText));
+            
+            Debug.Log($"{GetCallerData()}'{messageText}'");
         }
         
-        public static void LogFromAsync(string message, string caller, int  importanceLevel = 2)
+        public static void LogFromAsync(string messageText, [CallerMemberName] string caller = "", LogLevel importanceLevel = LogLevel.Verbose)
         {
-            if(IsDevelopmentModeEnabled == false) return;
+            if(!IsDevelopmentModeEnabled) return;
             
             if(LogDisplayLevel < importanceLevel) return;
+
+            if (string.IsNullOrEmpty(messageText)) throw new ArgumentNullException(nameof(messageText));
             
-            Debug.Log($"Class: '{caller}', Message: '{message}'");
+            
+            Debug.Log($"Class: '{caller}', Message: '{messageText}'");
         }
 
         /// <summary>
         /// Output a warning message to the console
         /// </summary>
-        /// <param name="message">Message to the console</param>
-        public static void LogWarning(string message)
+        /// <param name="messageText">Message to the console</param>
+        public static void LogWarning(string messageText)
         {
-            if(IsDevelopmentModeEnabled == false) return;
+            if(!IsDevelopmentModeEnabled) return;
 
-            Debug.LogWarning($"<color=yellow>{GetCallerData()}'{message}'</color>");
+            if (string.IsNullOrEmpty(messageText)) throw new ArgumentNullException(nameof(messageText));
+            
+            Debug.LogWarning($"<color=yellow>{GetCallerData()}'{messageText}'</color>");
         }
 
         /// <summary>
         /// Output a critical message to the console, Note that this pauses the editor when 'ErrorPause' is enabled.
         /// </summary>
-        /// <param name="message">Message to the console</param>
-        public static void LogError(string message)
+        /// <param name="messageText">Message to the console</param>
+        public static void LogError(string messageText)
         {
-            if(IsDevelopmentModeEnabled == false) return;
+            if(!IsDevelopmentModeEnabled) return;
 
-            Debug.LogError($"<color=red>{GetCallerData()}'{message}'</color>");
+            if (string.IsNullOrEmpty(messageText)) throw new ArgumentNullException(nameof(messageText));
+            
+            Debug.LogError($"<color=red>{GetCallerData()}'{messageText}'</color>");
         }
         
         /// <summary>
         /// It does not work correctly for asynchronous methods
         /// </summary>
         /// <returns></returns>
-        private static string GetCallerData()
-        {
-            StackTrace stackTrace = new StackTrace();
-            StackFrame callingFrame = stackTrace.GetFrame(2);
-
-            MethodBase callingMethod = callingFrame.GetMethod();
-            string callingClassName = callingMethod.ReflectedType.Name;
-
-            var callerData = $"Class: '{callingClassName}', Method: '{callingMethod.Name}', Message: ";
-            return callerData;
-        }
+        // private static string GetCallerData()
+        // {
+        //     StackTrace stackTrace = new StackTrace();
+        //     StackFrame callingFrame = stackTrace.GetFrame(2);
+        //     
+        //     if (callingFrame == null) return "Unknown caller: ";
+        //     
+        //     MethodBase callingMethod = callingFrame.GetMethod();
+        //     string callingClassName = callingMethod.ReflectedType.Name;
+        //
+        //     
+        //     string className = callingMethod?.ReflectedType?.Name ?? "UnknownClass";
+        //     string methodName = callingMethod?.Name ?? "UnknownMethod";
+        //     
+        //     var callerData = $"Class: '{className}', Method: '{methodName}', Message: ";
+        //     return callerData;
+        // }
+        
+        private static string GetCallerData() => 
+            new StackTrace(2, false).GetFrame(0)?.GetMethod() is MethodBase method 
+                ? $"Class: '{method.ReflectedType?.Name}', Method: '{method.Name}', Message: "
+                : "Unknown caller: ";
     }
 }
